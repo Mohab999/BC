41178fbb98a59ca1c9eec35f3b4a26e5b61113ac
Mohab999 <mohab@gmail.com>
2018-11-27 17:49:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 client.on('message', message => {
               if(!message.channel.guild) return;
     if(message.content.startsWith('^bc')) {
